d6946ec4ddaf874667d9b35552493ec220ee6589
sivan18-meet <sbmislovely@gmail.com>
2017-10-29 18:51:26 +0200

    my first commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+lista = [0,15,20,25]
+def flast ():
+	print lista[0]
+	print lista[-1]
+
+
+flast()
+r=[1,1,2,3,5,8,13,21,34,55,89]
+def carlos():
+	for i in r:
+		if i < 5:
+			print (i)
+carlos()
---
+++ y1session.py
@@ -0,0 +1,12 @@
+print ("carlos"*3)
+number1=15
+print number1
+number2=8
+print number2
+list1=[7,8,9]
+
+for a in list1:
+	print(a)
+	print(a*2)
+	
+
