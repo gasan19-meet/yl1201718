17a78c97b65c9b6fa8a629c0f4c40174faf19775
sivan18-meet <sbmislovely@gmail.com>
2017-10-29 19:24:55 +0200

    lab3

---
+++ turtleshapes.py
@@ -0,0 +1,14 @@
+import turtle
+#for i in range(4):
+    #turtle.forward(100)
+    #turtle.right(144)
+
+#turtle.forward(100)
+#turtle.mainloop()
+
+tutle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle
+
+
