248aeda83910b16fdf9822fa17a32bf64228c3cb
sivan18-meet <sbmislovely@gmail.com>
2017-11-05 18:28:21 +0200

    ahuevo

--- turtleshapes.py
+++ turtleshapes.py
@@ -4,11 +4,17 @@ import turtle
     #turtle.right(144)
 
 #turtle.forward(100)
-#turtle.mainloop()
 
-tutle.forward(40)
-turtle.right(90)
-turtle.forward(40)
-turtle
-
-
+###turtle.forward(40)
+###turtle.right(90)
+####turtle.forward(40)
+####turtle.right(45)
+####turtle.forward(40)
+###turtle.right(100)
+#####turtle.forward(40)
+######turtle.right(45)
+######turtle.forward(40)
+turtle.register_shape("cr7", ((0,0),(0,50),(50,50),(100,25),(50,0),(0,0)))
+turtle.getshapes()
+turtle.shape("cr7")
+turtle.mainloop()
