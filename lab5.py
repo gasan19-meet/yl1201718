5b6693259527a2b61d4ec43aa66cf06ecfac8291
sivan18-meet <sbmislovely@gmail.com>
2017-11-26 19:28:18 +0200

    kjhjhj

--- lab5.py
+++ lab5.py
@@ -1,10 +1,52 @@
-from turtle import turtle
-class Square (turtle):
-    def__init__(self,size):
-       turtle.__init__(self)
-       self.shapesize(size)
-       self.shape('square')
-    def random_color(self,color):
-    	self.color('green')
-square1=square(size=10)
-print(square1)
+from turtle import *
+import random
+
+# class Square(Turtle):
+#     def __init__(self,size):
+#        Turtle.__init__(self)
+#        self.shapesize(size)
+#        self.shape('square')
+
+#     def random_color(self):
+#     	self.color(red,green,blue)
+# square1=Square(10)
+# square1.random_color()
+
+
+# mainloop()
+
+                     #extaaaaaaaaaa
+
+
+
+class rectangle(Turtle):
+	def __init__(self,hight,width):
+		Turtle.__init__(self)
+		
+		register_shape('carlos' ,(0,0) (0,width) , (hight,width) , (0,hight) , (0,width))
+		self.shape('carlos')
+		self.setheading(90)
+rectangle1=rectangle(100,50)
+mainloop()
+colormode(255)
+red=random.randint(0,255)
+green=random.randint(0,255)
+blue=random.randint(0,255)
+class hexagon(Turtle):
+	def __init__(self,size):
+	  Turtle.__init__(self)
+	  self.shapesize(size)
+	  # self.speed(speed)
+	  self.begin_poly()
+	  self.color(red,green,blue)
+	  self.begin_fill()
+	  for i in range(0,6):
+		  self.forward(size)
+		  self.left(60)
+	  self.end_poly()
+	  self.end_fill()
+	  register_shape('hexagon', self.get_poly())
+	  self.shape('hexagon')
+hexegaon1=hexagon(100)
+speed(1)
+mainloop()
