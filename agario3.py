9eb4d108eaccf9a7676a69cf5bffea45bc35f50b
sivan18-meet <sbmislovely@gmail.com>
2018-01-21 19:24:24 +0200

    hi

--- agario3.py
+++ agario3.py
@@ -18,20 +18,30 @@ MAXIMUM_BALL_DX=5
 MINIMUM_BALL_DY=-5
 MAXIMUM_BALL_DY=5
 BALLS=[]
-for i in range(0,1320):
+for i in range (NUMBER_OF_BALLS):
 	screen_xpos=random.randint(int(-SCREEN_WIDTH+MAXIMUM_BALL_RADIUS),int(SCREEN_WIDTH-MAXIMUM_BALL_RADIUS))
 	screen_ypos=random.randint(int(-SCREEN_HEIGHT+MAXIMUM_BALL_RADIUS),int(SCREEN_HEIGHT-MAXIMUM_BALL_RADIUS))
 	ball_dx=random.randint(int(MINIMUM_BALL_DX),int(MAXIMUM_BALL_DX))
 	ball_dy=random.randint(int(MINIMUM_BALL_DY),int(MAXIMUM_BALL_DY))
 	ball_radius=random.randint(int(MINIMUM_BALL_RADIUS),int(MAXIMUM_BALL_RADIUS))
 	new_ball=circle(screen_xpos,screen_ypos,ball_dx,ball_dy,ball_radius)
-BALLS.append(new_ball)
+	BALLS.append(new_ball)
 def move_all_balls():
-	for i in range (0,2300):
+	for i in range (NUMBER_OF_BALLS):
 		new_ball.move(-300,300)
-def check_all_balls_collision(ball_1,ball_2):
+def collide(ball_1,ball_2):
+	if ball_1==ball_2:
+		return False
 	if ball_1.radius + ball_2.radius > math.sqrt(math.pow((ball_2.xcor()-ball_1.xcor()),2)+math.pow((ball_2.ycor()-ball_1.ycor()),2)):
-		return(true)
+			return True
 	else:
-		false
+		return False
+def check_all_balls_collision():
+	for ball_a in BALLS:
+		for ball_b in BALLS:
+			if collide(ball_a,ball_b):
+				collided_radius_a=ball_a.ball_radius
+				collided_radius_b=ball_b.ball_radius
+
+getscreen().update()
 mainloop()
